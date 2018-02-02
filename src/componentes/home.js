6649e2bf70da9391c5f0148ddd6f0650c97fbd24
hlebon <hanslgarcia@gmail.com>
2018-02-02 00:23:46 -0600

    feat: new format to display images

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -4,6 +4,23 @@ import * as ImageAPI from '../helps/api'
 import '../styles/grid.css'
 import logo from '../img/love.svg';
 
+function splitArrayByN (array, n) {
+
+    array.map( x => {
+        return x;
+    })
+    let results = []
+    let start = 0;
+    let end = n;
+    while( start <= array.length){
+        results.push(array.slice(start, end))
+        start = end
+        end = end + n
+    }
+
+    return results
+}
+
 class Home extends React.Component{
     state = {
         images: null
@@ -31,6 +48,14 @@ class Home extends React.Component{
 
     render(){
         const { images } = this.state
+        let images1, images2
+        if(images){
+            [ images1, images2 ] = splitArrayByN(images, 5)
+        }
+        
+        
+
+
         console.table(images)
         return (
             <div>
@@ -38,32 +63,43 @@ class Home extends React.Component{
                     searchImages={this.onSearchImage}
                 />
                 <section className={"section"}>
-                    <div className={"items"}>
-                    { images ? images.map((image) => 
-                        {
-                            return (
-                                <div key={image.id} className={"item"}>{
-                                    <div>
-                                        <img src={image.urls.small} alt=""/>
-                                        <div className={"item-info"}>
-                                            <small>Author: <span>{image.user.name}</span></small>
-                                            <div> <img src={logo} className={"heart"} alt=""/><span>{image.user.total_likes}</span></div>
+                    <div className={"items1"}>
+                    { images &&
+                    images1.map( image => {
+                        return (
+                            <div key={image.id} className={"item"}>
+                                <div>
+                                    <img src={image.urls.small} alt=""/>
+                                    <div className={"item-info"}>
+                                        <small>Author: <span>{image.user.name}</span></small>
+                                        <div>
+                                            <img src={logo} className={"heart"} alt=""/>
+                                            <span>{image.user.total_likes}</span>
                                         </div>
-                                    </div>
-                                    
-                                }</div>
-                            )
-                        })
-                    :   <div className="loader-container">
-                            <div className="loader">
-                                <div className="loading-box">
-                                    <div className="loading-inner-box">
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    }
-                        
+                        );
+                    })}    
+                    </div>
+                    <div className={"items2"}>
+                    { images &&
+                    images2.map( image => {
+                        return (
+                            <div key={image.id} className={"item"}>
+                                <div>
+                                    <img src={image.urls.small} alt=""/>
+                                    <div className={"item-info"}>
+                                        <small>Author: <span>{image.user.name}</span></small>
+                                        <div>
+                                            <img src={logo} className={"heart"} alt=""/>
+                                            <span>{image.user.total_likes}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}    
                     </div>
                 </section>
             </div>
